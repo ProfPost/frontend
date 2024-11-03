dfd78be851e6d3fee431d0fc65238921434d20ed
Hernan Mauriccio Gaston Berrospi Reyes <mauriccioreyes@gmail.com>
2024-11-02 23:00:09 -0500

    feat(front) implementación de rutas, layout y componente login

---
+++ src/app/pages/auth/auth.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { LoginComponent } from './login/login.component';
+
+export const authroutes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [{ path: 'login', component: LoginComponent }],
+  },
+];
