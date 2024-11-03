8ade0e5f4d985945c8ed4a4b66f5e7ec28cfc88c
Hernan Mauriccio Gaston Berrospi Reyes <mauriccioreyes@gmail.com>
2024-11-03 03:27:25 -0500

    feat(backend): implementacion de html y css para login, además de formularios reactivos, impletación de models, auth-request, auth-response

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private snackBar = inject(MatSnackBar);
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  controlHasError(control: string, error: string) {
+    return this.loginForm.controls[control].hasError(error);
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+    this.showSnackBar('Inicio de Sesión Exitoso');
+  }
+
+  private showSnackBar(message: string): void {
+    this.snackBar.open('Login Successful', 'Close', {
+      duration: 2000,
+      verticalPosition: 'top',
+    });
+  }
 }
