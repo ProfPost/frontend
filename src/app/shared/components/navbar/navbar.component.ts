03c8e7db986a87e694b18ce41a0c8e461599badb
Sergio <svelasquezr1@gmail.com>
2024-11-03 22:32:58 -0500

    feat(frontend) implementación de logout

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {AuthService} from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,5 +10,16 @@ import {RouterLink, RouterOutlet} from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  logout(): void {}
+  private authService = inject(AuthService);
+
+  isAuthenticated: boolean = false;
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.isAuthenticated = false;
+  }
 }
