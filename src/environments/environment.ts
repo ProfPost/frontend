4070df442dd99670852eaeac175d7d3ab8520925
Hernan Mauriccio Gaston Berrospi Reyes <mauriccioreyes@gmail.com>
2024-11-14 20:32:53 -0500

    implementacion de url de paypal, arreglo de imagenes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseURL: 'https://profpost-api-latest.onrender.com/api/v1',
-  paypalReturnUrl: 'http://localhost:4200/reader/subscription/checkout'
+  paypalReturnUrl: 'https://profpost-web-v1.netlify.app/reader/subscription/checkout'
 };
