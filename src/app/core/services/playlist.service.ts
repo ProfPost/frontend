7ce2282445fad593ab10f1b422a6d6567c721565
Hernan Mauriccio Gaston Berrospi Reyes <mauriccioreyes@gmail.com>
2024-11-27 13:00:55 -0500

    feat(front) implementaciplementacion de crud-playlist, se agrego a rutas de reader, se creo model para reques y response playlist, y su service

---
+++ src/app/core/services/playlist.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { PlaylistResponse } from '../../shared/models/playlist.response.model';
+import { PlaylistRequest } from '../../shared/models/playlistrequest.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlaylistService {
+  private baseURL = `${environment.baseURL}/playlist`;
+  private http = inject(HttpClient);
+
+  constructor() {}
+
+  getAllPlaylists(): Observable<PlaylistResponse[]> {
+    return this.http.get<PlaylistResponse[]>(`${this.baseURL}`);
+  }
+
+  getPlaylistById(id: number): Observable<PlaylistResponse> {
+    return this.http.get<PlaylistResponse>(`${this.baseURL}/${id}`);
+  }
+
+  createPlaylist(playlist: PlaylistRequest): Observable<PlaylistResponse> {
+    return this.http.post<PlaylistResponse>(this.baseURL, playlist);
+  }
+
+  updatePlaylist(
+    id: number,
+    playlist: PlaylistRequest
+  ): Observable<PlaylistResponse> {
+    return this.http.put<PlaylistResponse>(`${this.baseURL}/${id}`, playlist);
+  }
+
+  deletePlaylist(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseURL}/${id}`);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  baseURL: 'https://profpost-api-latest.onrender.com/api/v1',
+  production: false,
+  baseURL: 'http://localhost:8080/api/v1',
   paypalReturnUrl: 'http://localhost:4200/reader/subscription/checkout'
 };
